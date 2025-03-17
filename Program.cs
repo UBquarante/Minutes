f4b0a3a476aa6b9cecf34d5b29ebc1b6182f145d
votre-nom <votre-mail@entreprise.com>
2025-03-17 10:17:23 +0100

    fin de journée

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static (int, int) ConvertirMinutes(int nbMin)
+    {
+        int heures = nbMin / 60;
+        int minutes = nbMin % 60;
+        return (heures, minutes);
+    }
+
+    static void Main()
+    {
+        int[] valeursTest = { 90, 45, 120, 0, 200 };
+        foreach (int minutes in valeursTest)
+        {
+            var resultat = ConvertirMinutes(minutes);
+            Console.WriteLine($"{minutes} minutes = {resultat.Item1} heures et {resultat.Item2} minutes");
+        }
+    }
+}
