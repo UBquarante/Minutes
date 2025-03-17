3e1a9fa256aab3be0f21e61f5b34701de8bbe434
votre-nom <votre-mail@entreprise.com>
2025-03-17 11:33:30 +0100

    changement Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     static (int, int) ConvertirMinutes(int nbMin)
     {
@@ -9,11 +11,15 @@
 
     static void Main()
     {
-        int[] valeursTest = { 90, 45, 120, 0, 200 };
-        foreach (int minutes in valeursTest)
+        Console.Write("Entrez un nombre de minutes : ");
+        if (int.TryParse(Console.ReadLine(), out int nbMin))
         {
-            var resultat = ConvertirMinutes(minutes);
-            Console.WriteLine($"{minutes} minutes = {resultat.Item1} heures et {resultat.Item2} minutes");
+            var resultat = ConvertirMinutes(nbMin);
+            Console.WriteLine($"{nbMin} minutes = {resultat.Item1} heures et {resultat.Item2} minutes");
+        }
+        else
+        {
+            Console.WriteLine("Entrée invalide. Veuillez entrer un entier valide.");
         }
     }
 }
